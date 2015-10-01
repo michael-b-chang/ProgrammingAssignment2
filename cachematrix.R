4934a09b95074736d2c3ebb6fb0d76479aed1dc2
michael-b-chang <michael_b_chang@hotmail.com>
2015-10-01 16:05:44 +0800

    2015/10/1

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  rev <- NULL
+  set <- function(y) {
+    x <<- y
+    rev <<- NULL
+  }
+  get <- function() x
+  setrev <- function(s) rev <<- s
+  getrev <- function() rev
+  list(set = set, get = get,
+       setrev = setrev,
+       getrev = getrev)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getrev()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setrev(s)
+  s
 }
